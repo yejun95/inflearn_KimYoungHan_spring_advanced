72b08d55a72024564a76b535ae14ba3c6e4cbacd
jun <xodlf100@gmail.com>
2026-04-04 22:10:50 +0900

    feat: TemplateMethod -> Strategy 패턴 변환

---
+++ src/test/java/hello/advanced/trace/stratesy/code/strategy/Strategy.java
@@ -0,0 +1,5 @@
+package hello.advanced.trace.stratesy.code.strategy;
+
+public interface Strategy {
+    void call();
+}
---
+++ src/test/java/hello/advanced/trace/stratesy/code/strategy/StrategyLogic2.java
@@ -0,0 +1,11 @@
+package hello.advanced.trace.stratesy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StrategyLogic2 implements Strategy {
+    @Override
+    public void call() {
+        log.info("비즈니스 로직2 실행");
+    }
+}

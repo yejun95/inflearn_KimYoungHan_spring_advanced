a8dcc0cfbc4529d5f7b73af74e8caa310691a4d7
jun <xodlf100@gmail.com>
2026-04-07 00:22:18 +0900

    componentScan으로 구현 V3

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @Import({AppV1Config.class, AppV2Config.class})
-@SpringBootApplication(scanBasePackages = "hello.proxy.config")
+@SpringBootApplication(scanBasePackages = "hello.proxy.app.v3")
 public class ProxyApplication {
 
 	public static void main(String[] args) {

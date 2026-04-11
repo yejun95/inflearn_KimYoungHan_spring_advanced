71b1dda6e6cfb8d1b3159511e0307a4e4482c6ea
jun <xodlf100@gmail.com>
2026-04-11 15:39:37 +0900

    feat: V2 controller에 proxy 객체 적용 - 상속 2

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,7 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -11,8 +12,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
-@SpringBootApplication(scanBasePackages = "hello.proxy.app.v1")
+//@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app.v2")
 public class ProxyApplication {
 
 	public static void main(String[] args) {
